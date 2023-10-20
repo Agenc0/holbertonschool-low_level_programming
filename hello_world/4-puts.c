7ead028b8e8d472da3ce1ad335c7fea8890ef4c6
Agenc0 <tangentskate174@gmail.com>
2023-10-20 12:30:28 -0700

    fixed task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	char task = "\"Programming is like building a multilingual puzzle\n";
-
-	puts(task);
+	puts("\"Programming is like building a multilingual puzzle")
 
 	return(0);
 }
