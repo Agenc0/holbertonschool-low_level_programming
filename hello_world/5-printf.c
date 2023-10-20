5f40336ad820bcf665a7c166334f2f254f24cd8f
Agenc0 <tangentskate174@gmail.com>
2023-10-20 12:53:50 -0700

    added documentation for task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Prints with proper grammar, but the outcome is a piece of art,,
+ * 	followed by a new line.
+ *
+ *
+ * Return - Returns 0 at the end of the function.
+ */
+
 int main(void)
 {
 	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
