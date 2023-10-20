1e32b2b2f8d07a718ec58ad15d930dec8300351e
Agenc0 <tangentskate174@gmail.com>
2023-10-20 12:41:48 -0700

    added function desc in task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual puzzle, 
+ * 		  followed by a new line
+ *
+ * return - returns 0 at the end of the function
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
