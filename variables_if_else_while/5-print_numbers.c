00c2d600c1d036c1ea1bf6cf46d723acd6db894f
Agenc0 <tangentskate174@gmail.com>
2023-10-21 19:40:07 -0700

    added file for task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers 0 through 9
+ *
+ * Return: 0 if succesful
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	for (; num < 10; num++)
+		printf("%d", num);
+
+	printf('\n');
+	return (0);
+}
