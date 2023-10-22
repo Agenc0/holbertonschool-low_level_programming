c3fd179ad3432001f06785278cb9fd3690a70610
Agenc0 <tangentskate174@gmail.com>
2023-10-21 19:14:22 -0700

    added file for task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and
+ *	the alphabet in uppercase right after.
+ *
+ *
+ * Return: 0 if succesful
+ */
+
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
+
+	for (lower <= z; lower++)
+	{
+		putchar(lower);
+	}
+
+	for (upper <= Z; upper++)
+	{
+		putchar(upper);
+	}
+
+	putchar('\n');
+	return (0);
+
+}
