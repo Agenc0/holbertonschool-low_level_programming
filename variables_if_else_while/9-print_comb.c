77a9cc6de1152053a2165270db2cbd3e45a764f6
Agenc0 <tangentskate174@gmail.com>
2023-10-21 20:37:13 -0700

    fix task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,10 +11,10 @@ int main(void)
 {
 	int num = 0;
 
-	for (; num <= 9; num++)
+	for (; num <= 10; num++)
 	{
 		putchar(num + '0');
-		if (num <= 57)
+		if (num <= 9)
 		{
 			putchar(44);
 			putchar(32);
