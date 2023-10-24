0633113c1374865d08bffa7b9f357cc604f8db99
Agenc0 <tangentskate174@gmail.com>
2023-10-23 19:19:01 -0700

    added file for task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.hs"
+
+/**
+ * _islower - Checks wether 'c' is a lowercase character
+ * if it is, returns 1, if not, returns 0
+ *
+ * Return: 1 if lowercase, 0 if not
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <='z')
+		return (1);
+	else
+		return (0);
+}
