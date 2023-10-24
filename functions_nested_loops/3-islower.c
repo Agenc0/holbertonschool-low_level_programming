945d91d0d3e0c88b98a738e390d2b6bc3647e47c
Agenc0 <tangentskate174@gmail.com>
2023-10-23 19:21:21 -0700

    fixed coding style and documentation for task 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -4,12 +4,14 @@
  * _islower - Checks wether 'c' is a lowercase character
  * if it is, returns 1, if not, returns 0
  *
+ * c - character to be checked
+ *
  * Return: 1 if lowercase, 0 if not
  */
 
 int _islower(int c)
 {
-	if (c >= 'a' && c <='z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
