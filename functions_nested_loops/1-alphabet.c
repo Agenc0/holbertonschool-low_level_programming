ff29c5926cdd43f51b51e780164df4930d04a1bd
Agenc0 <tangentskate174@gmail.com>
2023-10-23 18:57:14 -0700

    added file for task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase,
+ * followed by a new line
+ *
+ * Return: void 
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	for(; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
