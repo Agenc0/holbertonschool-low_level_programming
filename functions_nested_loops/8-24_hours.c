978c0d9139e648664e5db90ebde366bc0007eeb0
Agenc0 <tangentskate174@gmail.com>
2023-10-23 20:32:33 -0700

    added file for task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the 24 hour clock,
+ * from 00:00 to 23:59
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int hour = 0;
+
+	for (; hour <= 23; hour++)
+	{
+		int minute = 0;
+
+		for (; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n')
+		}
+	}
+}
