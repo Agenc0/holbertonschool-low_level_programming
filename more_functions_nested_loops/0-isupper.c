fbbcbe9f2dd978cf59ab192ebb69ba47595a0bdf
Agenc0 <tangentskate174@gmail.com>
2023-10-25 18:34:50 -0700

    added function for task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,17 @@
-#include
+#include "main.h"
+
+/**
+ * _isupper - Checks if a character is an uppercase letter
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if uppercase, 0 if else
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+/**
+ * Header file for project more functions, nested loops
+ * contains declaration of various functions
+ */
+
+int _putchar(char c);
+int _isupper(int c);
