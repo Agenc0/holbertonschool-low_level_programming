3923418b1256068d395dc8cbca301e78eadfbe3f
Agenc0 <tangentskate174@gmail.com>
2023-10-25 19:50:54 -0700

    added file for task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square with the # character,
+ * which size depends on input
+ *
+ * @size: length and width of the square
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int height;
+
+		for (; height < size; height++)
+		{
+			int width;
+			
+			for (; width < size; width++)
+				_putchar('#');
+
+			_putchar('\n');
+		}
+	}
+	else
+		_printchar('\n');
+}
