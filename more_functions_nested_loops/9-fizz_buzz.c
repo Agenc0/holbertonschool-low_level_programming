c94b8a51f1f07718665e772cf13d500d3a44ee46
Agenc0 <tangentskate174@gmail.com>
2023-10-25 20:44:37 -0700

    added task 9 file

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints numbers from 1 to 100, with multiples
+ * of 3 printing as "Fizz", multiples of 5 printing as
+ * "Buzz", and multiples of both printing as "FizzBuzz"
+ *
+ * Return: 0 if succesful
+ */
+
+int main(void)
+{
+	int digit = 1;
+
+	for (; digit <= 100; digit++)
+	{
+		if ((digit % 3) == 0)
+			printf("Fizz");
+
+		else if ((digit % 5) == 0)
+			printf("Buzz");
+
+		else if ((digit % 3) == 0 && (digit % 5) == 0)
+			printf("FizzBuzz");
+
+		else
+			printf("%d", digit);
+
+		printf(" ");
+	}
+	
+	printf("\n");
+
+	return (0);
+}
