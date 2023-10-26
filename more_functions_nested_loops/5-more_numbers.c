3a8f76da67f3509c6eca6a379f595829c1569e35
Agenc0 <tangentskate174@gmail.com>
2023-10-25 19:10:52 -0700

    added file for task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14
+ * 10 times, each separated by a new line
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int count = 0;
+
+	for (; count <= 9; count++)
+	{
+		int digit = 0;
+
+		for (; digit <= 14; num++)
+		{
+			if (digit > 9)
+				_putchar((digit / 10) + '0');
+			
+			_putchar((digit % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
