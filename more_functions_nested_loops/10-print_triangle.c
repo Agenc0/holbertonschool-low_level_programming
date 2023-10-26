2a2a5c721af7c48d80478891758273801e6181ca
Agenc0 <tangentskate174@gmail.com>
2023-10-25 21:17:00 -0700

    added file for task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle/pyramid, which size
+ * is based on input
+ *
+ * @size: Number of steps/height of the triangle/pyramid
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int var1 = 1;
+
+			for (; var1 <= size; var1++)
+			{
+				int spaces = size - var1;
+				int bricks = 0;
+
+				for (; spaces > 0; spaces--)
+					_putchar(' ');
+
+				for (; bricks < var1; bricks++)
+					_putchar('#');
+
+				_putchar('\n');
+			}
+	}
+
+	_putchar('\n');
+}
