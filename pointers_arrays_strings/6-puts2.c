789855f2886686d7f4ae735e7fabf808887ab084
Agenc0 <tangentskate174@gmail.com>
2023-10-30 21:53:34 -0700

    added task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every odd character in a string
+ *
+ * @str: string to get characters from
+ *
+ * Returns: void
+ */
+
+void puts2(char *str)
+{
+	int length = 0;
+	int var = 0;
+
+	while (str[var++])
+		length++;
+
+	for (; var < length; length += 2)
+		_putchar(str[var]);
+
+	_putchar('\n');
+}
