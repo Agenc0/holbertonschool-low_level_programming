ae2ba7b42e44b311249343ea17c3d98c9b077113
Agenc0 <tangentskate174@gmail.com>
2023-10-30 21:44:14 -0700

    added task 5;

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string to be reversed
+ *
+ * Return: reversed string
+ */
+
+void rev_string(char *s)
+{
+	int length = 0;
+	int var = 0;
+
+	while (s[var++])
+		length++;
+
+	var = length - 1
+
+	for (; var >= length / 2; var--)
+	{
+		int tmp = s[var];
+		s[var] = s[length - var - 1];
+		s[length - var - 1] = tmp;
+	}
+}
