9a9b48fa000e49a3e0574dc1f8aa67eaa902c5e7
Agenc0 <tangentskate174@gmail.com>
2023-10-30 22:25:27 -0700

    added task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string, including null byte
+ * to another buffer
+ *
+ * @dest: destination buffer to copy to
+ *
+ * @src: source to copy from / string to copy
+ *
+ * Return: a pointer to destination string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int var = 0;
+
+	while (src[var])
+	{
+		dest[var] = src[var];
+		var++;
+	}
+
+	return (dest);
+}
