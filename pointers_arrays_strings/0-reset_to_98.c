322516acbd0db2d4c3efa98526a91e048628f302
Agenc0 <tangentskate174@gmail.com>
2023-10-30 20:59:24 -0700

    fixed doc and style issues

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,8 @@
 /**
  * reset_to_98 - takes a pointer to an int, and updates
  * the value of said int to 98.
- * 
- * @*n: pointer to the int
+ *
+ * @n: pointer to the int
  *
  * Return: void
  */
