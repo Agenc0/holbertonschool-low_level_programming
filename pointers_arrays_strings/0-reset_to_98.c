b4d059475477a9ee68636bde4db99c061557a690
Agenc0 <tangentskate174@gmail.com>
2023-10-30 20:58:13 -0700

    added a bunch of stuff for the new project

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int, and updates
+ * the value of said int to 98.
+ * 
+ * @*n: pointer to the int
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+/**
+ * Header file for project pointers arrays and strings
+ * contains declarations of functions
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
