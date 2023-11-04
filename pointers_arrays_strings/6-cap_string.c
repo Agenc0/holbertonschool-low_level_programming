bd714e094adb5ca3f239b454359be590f19dd636
Agenc0 <tangentskate174@gmail.com>
2023-11-03 19:32:04 -0700

    added task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ *
+ * @str: string to modify
+ *
+ * Return: pointer to modified string
+ */
+
+char *cap_string(char *str)
+{
+	int var = 0;
+
+	while (str[var])
+	{
+		while (!(str[var] >= 'a' && str[var] <= 'z'))
+			var++;
+
+		if (str[var] - 1 == ' ' ||
+				str [var] - 1 == '\t' ||
+				str [var] - 1 == '\n' ||
+				str [var] - 1 == ',' ||
+				str [var] - 1 == ';' ||
+				str [var] - 1 == '.' ||
+				str [var] - 1 == '!' ||
+				str [var] - 1 == '?' ||
+				str [var] - 1 == '"' ||
+				str [var] - 1 == '(' ||
+				str [var] - 1 == ')' ||
+				str [var] - 1 == '{' ||
+				str [var] - 1 == '}' ||
+				var == 0)
+			str[var] -= 32;
+
+		var++;
+	}
+
+	return (str);
+}
