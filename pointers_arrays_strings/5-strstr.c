09dff8c0ff0729b18c73f0d7daf111c35865243b
Agenc0 <tangentskate174@gmail.com>
2023-11-04 21:06:23 -0700

    added task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring in a given string
+ *
+ * @haystack: string to be searched
+ * @needle: substring to search for
+ *
+ * Return: pointer to the beggining of substring if found, null if else
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	if (needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		int var = 0;
+
+		if (haystack[var] == needle[var])
+		{
+			do {
+				if (needle[var + 1] == '\0')
+					return (haystack);
+
+				var++;
+
+			} while (haystack[var] == needle[var]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
