7252ffb127cc15d2e741da69bec8ab94dc5b59a7
Agenc0 <tangentskate174@gmail.com>
2023-11-06 18:22:27 -0800

    added task 0

---
+++ recursion/README.md
@@ -0,0 +1,1 @@
+directory for recursion related projects
---
+++ recursion/main.h
@@ -0,0 +1,7 @@
+/**
+ * header file for recursion related projects
+ * contains declarations of functions
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
