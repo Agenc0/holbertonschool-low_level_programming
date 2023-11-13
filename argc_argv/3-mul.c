4b4c29d3e10a39692804a4f6347d2063257f1b48
Agenc0 <tangentskate174@gmail.com>
2023-11-12 17:01:47 -0800

    added task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the product of two numbers
+ *
+ * @argc: # of arguments given
+ * @argv: array of pointers to strings (arguments)
+ *
+ * Return: 0 if succesful, prints "Error" and returns 1 if else
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
