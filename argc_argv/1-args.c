c982e65772cc6c01e773bfb9308f2f53a1d03f05
Agenc0 <tangentskate174@gmail.com>
2023-11-12 16:48:10 -0800

    added task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed onto it
+ *
+ * @argc: number of arguments given
+ * @argv: array of pointers to strings (arguments)
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
