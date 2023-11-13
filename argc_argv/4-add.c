1b1cac98c9415100699ab8795793297e6bbc8edd
Agenc0 <tangentskate174@gmail.com>
2023-11-12 17:19:04 -0800

    added task 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the addition of positive numbers
+ *
+ * @argc: # of arguments given
+ * @argv: array of pointers to strings (arguments)
+ *
+ * Return: 0 if successful, print "Error" and return 1 if else
+ */
+
+int main(int argc, char *argv[])
+{
+	int var = 1;
+	int sum = 0;
+
+	for (; i < argc; i++)
+	{
+		int var1 = 0;
+
+		for (; argv[var][var1]; var1++)
+		{
+			if (argc[var][var1] < '0' || argc[var][var1] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[var]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
