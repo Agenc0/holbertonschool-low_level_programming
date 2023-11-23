38ef57fa0b1fa54a819e7ee4848768b4147eb8b5
Agenc0 <tangentskate174@gmail.com>
2023-11-22 20:22:43 -0800

    added t4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,70 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * _strcpy - copies a string
+ *
+ * @s: string to copy
+ *
+ * Return: pointer to string copy
+ */
+
+char *_strcpy(char *s)
+{
+	int var;
+	char *p;
+
+	for (var = 0; s[var]; var++)
+		;
+
+	p = malloc(var + 1);
+
+	if (p)
+	{
+		for (; var >= 0; var--)
+			p[var] = s[var];
+	}
+
+	return (p);
+}
+
+/**
+ * new_dog - creates a new dawg
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: dog struct if successful, NULL if else
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+
+	p = malloc(sizeof(*p));
+
+	if (p == NULL)
+		return (NULL);
+
+	p->name = _strcpy(name);
+
+	if (p->name == NULL)
+	{
+		free (p);
+		return (NULL);
+	}
+
+	p->age = age;
+
+	p->owner = _strcpy(owner);
+
+	if (p->owner == NULL)
+	{
+		free (p->name);
+		free (p);
+		return (NULL);
+	}
+
+	return (p);
+}
