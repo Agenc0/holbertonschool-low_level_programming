fb158dd3bcbf82cb8133f0296f0d2cd3aacbaad4
Agenc0 <tangentskate174@gmail.com>
2023-11-25 16:41:49 -0800

    added t1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function through every element
+ * of a given array
+ *
+ * @array: array to execute on
+ * @size: size of the array
+ * @action: function to execute
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t var;
+
+	if (array && action)
+	{
+		for (var = 0; var < size; var++)
+			action(array[var]);
+	}
+}
