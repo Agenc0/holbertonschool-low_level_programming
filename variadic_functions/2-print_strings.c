897a5a60e9b96391a64905e06719fa7f2ab44340
Agenc0 <tangentskate174@gmail.com>
2023-11-26 17:26:25 -0800

    added t2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints a sequence of strings, followed by a newline
+ *
+ * @separator: string to print between strings
+ * @n: # of strings
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...);
+{
+	va_list args;
+	unsigned int var;
+	char *s;
+
+	va_start(args, n);
+
+	for (var = 0; var < n; var++)
+	{
+		s = va_arg(args, char *);
+
+		if (!s)
+			s = "(nil)";
+
+		printf("%s", s);
+
+		if (separator && var != n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(args);
+}
