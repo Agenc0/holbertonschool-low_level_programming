a79d58f11de4654984a13871e0bff52ad43c8142
Agenc0 <tangentskate174@gmail.com>
2023-12-02 21:04:20 -0800

    added t4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list of type list_t
+ *
+ * @head: pointer to list head
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
