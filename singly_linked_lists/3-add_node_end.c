7e295597bf3b6a0bb913baffbb594fae19ddd3de
Agenc0 <tangentskate174@gmail.com>
2023-12-02 20:58:32 -0800

    added t3

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node_end - appends a node at the end of a list
+ * of type list_t
+ *
+ * @head: pointer to head of list
+ * @str: string to add as value for new node
+ *
+ * Return: pointer to new node if successful, NULL if else
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *end;
+	char *temp;
+	unsigned int i;
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	temp = strdup(str);
+
+	if (str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	for (i = 0; str[i];)
+		i++;
+
+	new_node->str = temp;
+	new_node->len = i;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		end = *head;
+		while (end->next != NULL)
+			end = end->next;
+		last->next = new_node;
+	}
+
+	return (*head);
+}
