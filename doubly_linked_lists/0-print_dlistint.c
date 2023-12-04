25149c4154214a7b8090b503fe105a8e4cad8028
Agenc0 <tangentskate174@gmail.com>
2023-12-03 19:14:58 -0800

    fix t0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_dtlistint - prints all elements in a list of type
+ * print_dlistint - prints all elements in a list of type
  * dtlistint_t
  *
  * @h: pointer to head of list
@@ -9,7 +9,7 @@
  * Return: # of nodes in list
  */
 
-size_t print_dtlistint(const dtlistint_t *h)
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
 
