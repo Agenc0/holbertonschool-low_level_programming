6028eee3c5c4cb2dc92fe3c75d4e55b9ea9e10de
Agenc0 <tangentskate174@gmail.com>
2023-12-16 18:09:02 -0800

    added t1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ *
+ * @filename: name for the file
+ * @text_content: string content for the file
+ *
+ * Return: 1 if successful, -1 if else
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (; text_content[len])
+			len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
