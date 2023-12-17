2198c8f44a95918da74360560a880cf49a3d95d5
Agenc0 <tangentskate174@gmail.com>
2023-12-17 09:28:25 -0800

    fixed t3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,38 +12,47 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *tuple;
-	char *value_cpy = NULL;
+	unsigned long int index, i;
+	hash_node_t *new_node;
+	char *value_cpy;
 
-	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
+
+	value_cpy = strdup(value);
+	if (value_cpy == NULL)
+		return (0);
+
 	index = key_index((const unsigned char *)key, ht->size);
-	for (tuple = ht->array[index]; tuple != NULL; tuple = tuple->next)
+	for (i = index; ht->array[i]; i++)
 	{
-		if (strcmp(tuple->key, key) == 0)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			value_cpy = strdup(value);
-			if (value_cpy == NULL)
-				return (0);
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_cpy;
+			return (1);
 		}
-		free(tuple->value);
-		tuple->value = value_cpy;
-		return (1);
 	}
-	tuple = malloc(sizeof(*tuple));
-	if (tuple == NULL)
-		return (0);
-	tuple->key = strdup(key);
-	tuple->value = strdup(value);
-	if (tuple->key == NULL || tuple->value == NULL)
+
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
 	{
-		free(tuple->key);
-		free(tuple->value);
-		free(tuple);
+		free(value_cpy);
 		return (0);
 	}
-	tuple->next = ht->array[index];
-	ht->array[index] = tuple;
+
+	new_node->key = strdup(key);
+
+	if(new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	new_node->value = value_cpy;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
 	return (1);
 }
